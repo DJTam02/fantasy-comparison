5ba5584785ab5fff9825b9ab457370a5ea7a1900
Jacob Tam <jacobtam2002@gmail.com>
2023-01-12 00:17:44 -0500

    fix bug

--- src/constants/authentication.ts
+++ src/constants/authentication.ts
@@ -3,4 +3,4 @@ export const REQUEST_AUTH_URL = "https://api.login.yahoo.com/oauth2/request_auth
 export const REFRESH_TOKEN_COOKIE = "refresh_token";
 export const ACCESS_TOKEN_COOKIE = "access_token";
 
-export const REDIRECT_URI = window.location.origin + (window.location.pathname === "/" ? "" : window.location.pathname) + "/authenticate";
+export const REDIRECT_URI = window.location.href + "authenticate";
