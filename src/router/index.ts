98f3dc336ff78e049d0cdf72a0b3f138050a13d6
Jacob Tam <jacobtam2002@gmail.com>
2023-01-12 00:54:32 -0500

    fix routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory((import.meta.env.PROD ? "/fantasy-comparison" : "") + import.meta.env.BASE_URL),
   routes: [
     {
       path: (import.meta.env.PROD ? "/fantasy-comparison" : "") + "/change-secret",
