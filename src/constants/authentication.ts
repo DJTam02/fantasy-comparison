852947844aa874c162d031a10e969d394f5a8850
DJTam02 <jacobtam2002@gmail.com>
2023-12-08 15:34:06 -0500

    fixed redirect uri

--- src/constants/authentication.ts
+++ src/constants/authentication.ts
@@ -4,7 +4,7 @@ export const GET_TOKEN_URL = "https://api.login.yahoo.com/oauth2/get_token";
 export const REFRESH_TOKEN_COOKIE = "refresh_token";
 export const ACCESS_TOKEN_COOKIE = "access_token";
 
-export const REDIRECT_URI = window.location.href + "authenticate";
+export const REDIRECT_URI = "https://jacobtam.ca/fantasy-comparison/authenticate";
 
 export const ALPHA_NUMERIC_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 export const HASH_METHOD = "S256";
